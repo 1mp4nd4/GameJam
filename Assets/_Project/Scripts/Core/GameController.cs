7ac0623374951c933db4ac9fe36c8ee434cada23
AlexPallottini <50850522+AlexPallottini@users.noreply.github.com>
2024-11-16 14:18:32 -0300

    Added Game controller
    Script and bootstrapper scene

---
+++ Assets/_Project/Scripts/Core/GameController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController _instance;
+    public static GameController Instance => _instance ??= new GameObject("GameController").AddComponent<GameController>();
+
+    //Esto y el diccionario están para no hacerse quilombo si el día de mañana querés agregar una escena más o querés hacer alguna otra cosa. Las agregas acá y lo cambias todo desde el método.
+    public enum Scenes
+    {
+        None = 0,
+        MainMenu = 1,
+        Gameplay = 2,
+    }
+
+    private Dictionary<Scenes,string> scenesDict = new Dictionary<Scenes,string>()
+    {
+        { Scenes.MainMenu, "MainMenu" },
+        { Scenes.Gameplay, "Gameplay" }
+    };
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(Instance.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    private void Start()
+    {
+        ChangeScene(Scenes.MainMenu);
+    }
+
+    // Con esto cambias de escena je
+    public void ChangeScene(Scenes scene)
+    {
+        Debug.Log("test");
+        if(scenesDict.TryGetValue(scene, out string sceneName))
+        {
+            Debug.Log(sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
